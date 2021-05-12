dcac6ffbbfa3787a07e11211bc4e719f84c2c9d3
stefzouille <contact.lokcy@gmail.com>
2021-05-12 14:46:16 +0200

    add

--- SmartWallet.sol
+++ SmartWallet.sol
@@ -7,84 +7,101 @@ pragma solidity ^0.8.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Address.sol";
 import "./Ownable.sol";
 
-contract SmartWallet is Ownable {
+// Deployed at 0x74d2d2a340A15811b9D2F076f6b8834a343e2D94 on Goerli
+contract SmartWallet {
     // library usage
     using Address for address payable;
 
     // State variables
-    mapping(address => uint256) private _balances;
-    mapping (address => mapping (address => uint256)) private _allowances;
-    mapping(address => bool) private _vipMembers;
-    uint256 private _tax;
+    address private _owner;
+    uint256 private _percentage;
     uint256 private _profit;
     uint256 private _totalProfit;
+    mapping(address => uint256) private _balances;
+    mapping(address => uint256) private _depositTime;
+    mapping(address => bool) private _whiteList;
+    mapping(address => mapping(address => uint256)) private _allowances;
 
     // Events
-    event Deposited(address indexed sender, uint256 amount);
-    event Withdrew(address indexed recipient, uint256);
-    event Transfered(address indexed sender, address indexed recipient, uint256 amount);
-    event VipSet(address indexed account, bool status);
-    // Exercice 2
-    // Ajouter un event Approval qui sera emit des qu'un approval aura été autorisé
-    // il faudra qu'on y retrouve comme information l'ower des fonds, le spender et la somme autorisée
-    // à etre depensée.
+    event Deposited(address indexed sender, uint256 amount, uint256 epochTime);
+    event Withdrew(address indexed recipient, uint256, uint256 epochTime);
+    event Transfered(
+        address indexed sender,
+        address indexed recipient,
+        uint256 amount,
+        uint256 epochTime
+    );
+    event Approval(
+        address indexed owner,
+        address indexed sender,
+        uint256 amount
+    );
 
     // constructor
-    constructor(address owner_, uint256 tax_) Ownable(owner_) {
-        require(tax_ >= 0 && tax_ <= 100, "SmartWallet: Invalid percentage");
-        _tax = tax_;
+    constructor(uint256 percentage_, address owner_) {
+        require(
+            percentage_ >= 0 && percentage_ <= 100,
+            "SmartWallet: Invalid percentage"
+        );
+        _owner = owner_;
+        _percentage = percentage_;
     }
 
     // modifiers
     // Le modifier onlyOwner a été défini dans le smart contract Ownable
+    modifier onlyMembers() {
+        require(
+            block.timestamp > _depositTime[msg.sender] + 300,
+            "SmartWallet: You are not a members, please deposit and wait 5min"
+        );
+        _; // +300 correspond à 5min
+    }
 
+    modifier onlyOwner() {
+        require(
+            msg.sender == _owner,
+            "Ownable: Only owner can call this function"
+        );
+        _;
+    }
 
     // Function declarations below
     receive() external payable {
         _deposit(msg.sender, msg.value);
     }
 
-    fallback() external {
-
-    }
+    fallback() external {}
 
     function deposit() external payable {
         _deposit(msg.sender, msg.value);
     }
 
-
-    function withdraw() public {
+    function withdrawAll() public {
         uint256 amount = _balances[msg.sender];
         _withdraw(msg.sender, amount);
     }
 
-    function withdrawAmount(uint256 amount) public {
+    function withdraw(uint256 amount) public onlyMembers {
         _withdraw(msg.sender, amount);
     }
 
-    // Exerice 3:
-    // Implémenter cette fonction pour que le msg.sender autorise spender à
-    // dépenser en son nom l'equivalent de amount
-    // il faudra manipuler pour cela le double mapping _allowances
-    function approve(address spender, uint256 amount) public {
-    }
-
-    function transfer(address recipient, uint256 amount) public {
-        require(_balances[msg.sender] > 0, "SmartWallet: can not transfer 0 ether");
-        require(_balances[msg.sender] >= amount, "SmartWallet: Not enough Ether to transfer");
-        require(recipient != address(0), "SmartWallet: transfer to the zero address");
+    function doTransfer(address recipient, uint256 amount) public onlyMembers {
+        require(
+            _balances[msg.sender] > 0,
+            "SmartWallet: can not transfer 0 ether"
+        );
+        require(
+            _balances[msg.sender] >= amount,
+            "SmartWallet: Not enough Ether to transfer"
+        );
+        require(
+            recipient != address(0),
+            "SmartWallet: transfer to the zero address"
+        );
         _balances[msg.sender] -= amount;
         _balances[recipient] += amount;
-        emit Transfered(msg.sender, recipient, amount);
+        emit Transfered(msg.sender, recipient, amount, block.timestamp);
     }
-
-    // Exercice 4:
-    // Implémenter cette fonction pour que le msg.sender puisse transférer des fonds "amount" depuis "from" vers "to"
-    // Il faudra emettre un event Transfered si le transfer est effectué avec succès
-    function transferFrom(address from, address to, uint256 amount) public {
-        // ecriture dans un registre comptable
-    }
-
 
     function withdrawProfit() public onlyOwner {
         require(_profit > 0, "SmartWallet: can not withdraw 0 ether");
@@ -93,26 +110,40 @@ contract SmartWallet is Ownable {
         payable(msg.sender).sendValue(amount);
     }
 
-    function setTax(uint256 tax_) public onlyOwner {
-        require(tax_ >= 0 && tax_ <= 100, "SmartWallet: Invalid percentage");
-        _tax = tax_;
+    function setTax(uint256 percentage_) public onlyOwner {
+        require(
+            percentage_ >= 0 && percentage_ <= 100,
+            "SmartWallet: Invalid percentage"
+        );
+        _percentage = percentage_;
     }
 
-    function setVip(address account) public onlyOwner {
-        _vipMembers[account] = !_vipMembers[account];
-        emit VipSet(account, _vipMembers[account]);
+    function setWhiteList(address account) public onlyOwner {
+        _whiteList[account] = !_whiteList[account];
     }
 
+    function approve(address account, uint256 amount) public {
+        _allowances[msg.sender][account] = amount;
+        emit Approval(msg.sender, account, amount);
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public {
+        require(
+            _allowances[from][msg.sender] >= amount,
+            "SmartWallet: You don't have any funds allowed anymore."
+        );
+        _allowances[from][msg.sender] -= amount;
+        _balances[from] -= amount;
+        _balances[to] += amount;
+        emit Transfered(from, to, amount, block.timestamp);
+    }
 
     function balanceOf(address account) public view returns (uint256) {
         return _balances[account];
-    }
-
-    // Exercice 1
-    // Implementer cette fonction pour qu'elle nous retourne ce que spender peut
-    // encore dépenser en tant owner_.
-    function allowance(address owner_, address spender) public view returns (uint256 remaining) {
-
     }
 
     function total() public view returns (uint256) {
@@ -120,45 +151,70 @@ contract SmartWallet is Ownable {
     }
 
     function tax() public view returns (uint256) {
-        return _tax;
+        return _percentage;
     }
 
-    function profit() public view returns(uint256) {
+    function profit() public view returns (uint256) {
         return _profit;
     }
 
-    function totalProfit() public view returns(uint256) {
+    function totalProfit() public view returns (uint256) {
         return _totalProfit;
     }
 
-    function isVipMember(address account) public view returns (bool) {
-        return _vipMembers[account];
+    // fonction voir le temps
+    function whatTime() public view returns (uint256) {
+        return block.timestamp;
+    }
+
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    function isOnWhiteList(address account) public view returns (bool) {
+        return _whiteList[account];
+    }
+
+    function allowance(address owner_, address spender)
+        public
+        view
+        returns (uint256)
+    {
+        return _allowances[owner_][spender];
     }
 
     function _deposit(address sender, uint256 amount) private {
         _balances[sender] += amount;
-        emit Deposited(sender, amount);
+        _depositTime[sender] = block.timestamp;
+        emit Deposited(sender, amount, block.timestamp);
     }
 
     function _withdraw(address recipient, uint256 amount) private {
-        require(_balances[recipient] > 0, "SmartWallet: can not withdraw 0 ether");
-        require(_balances[recipient] >= amount, "SmartWallet: Not enough Ether");
-        // version de john avec ternaire
-        // uint256 fees = _vipMembers[recipient] ? 0 : _calculateFees(amount, _tax);
-        uint256 fees = 0;
-        if(_vipMembers[recipient] != true) {
-            fees = _calculateFees(amount, _tax);
-        }
+        require(
+            _balances[recipient] > 0,
+            "SmartWallet: can not withdraw 0 ether"
+        );
+        require(
+            _balances[recipient] >= amount,
+            "SmartWallet: Not enough Ether"
+        );
+        uint256 fees =
+            _whiteList[recipient] == true
+                ? 0
+                : _calculateFees(amount, _percentage);
         uint256 newAmount = amount - fees;
         _balances[recipient] -= amount;
         _profit += fees;
         _totalProfit += fees;
         payable(msg.sender).sendValue(newAmount);
-        emit Withdrew(msg.sender, newAmount);
+        emit Withdrew(msg.sender, newAmount, block.timestamp);
     }
 
-
-    function _calculateFees(uint256 amount, uint256 tax_) private pure returns (uint256) {
-        return amount * tax_ / 100;
+    function _calculateFees(uint256 amount, uint256 percentage_)
+        private
+        pure
+        returns (uint256)
+    {
+        return (amount * percentage_) / 100;
     }
 }
